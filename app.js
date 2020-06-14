f13fb59806cf807d4abc61792aa7b54d0a7e8715
Ta-hash <talhamuhamnad7@gmail.com>
2020-06-14 17:30:18 +0500

    Now ready to commit

---
+++ app.js
@@ -0,0 +1,717 @@
+// // 1. Write a script to greet your website visitor using JS alert box. 
+// alert("welcome to my website")
+
+// // 2. Write a script to display following message on your web page
+
+// alert(" Error! please enter a valid password")
+
+// // 3. Write a script to display following message on your web page: (Hint : Use line break) 
+
+// alert(" Welcome to js land...\nHappy Coding!")
+ 
+// // 4. Write a script to display following messages in sequence: 
+
+// alert(" Welcome to js land...")
+
+
+// alert(" Happy Coding!\nPrevent this page from additional dialogs")
+
+// // 5. Generate the following message through browser’s developer console: 
+
+// // console.log(" hello.. i can run js through my browser Console")
+ 
+// // chp 2 variables
+// // 1. Declare a variable called username.
+// //  2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// // var x = "Talha";
+// // var y ="haroon";
+// // var z = x+y;
+// // alert(z)
+
+// // 3. Write script to a) Declare a JS variable,
+// //  titled message. b) Assign “Hello World” to variable message c) Display the message in alert box. 
+
+// // var a = "hello";
+// // var b = "world";
+// // var c = a+b;
+// // alert(c)
+
+// // 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes. 
+
+
+// // var name = "Talha"
+// // var fullname = "haroon"
+// // var c = name+fullname ;
+// // alert(c)
+
+// // var age = 15;
+// // var years = "years old"
+// // var c = age+years;
+// // alert(c)
+
+// var a = "certified  mobile "
+// var b = "application development"
+// var c = a+b ;
+// alert (c)
+
+
+
+// var a = "pizza\npizz\npiz\npi\np";
+// alert(a)
+ 
+
+// var email = "haroontalha621@gmail.com";
+// alert(email)
+
+
+// var a = "i am trying to learn from the book\na smart way to learn javascript";
+// alert(a);
+
+  
+// 8. Write a script to display this in browser through JS 
+
+// document.write("yah! i can write html content through javascript")
+
+
+// 9. Store following string in a variable and show in alert and browser through JS 
+ 
+//  var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//  alert(a);
+
+
+
+
+// chp 3
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box. 
+
+// var age =  "i am 18 years old";
+// alert(age)
+
+
+// 2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page. 
+// Show his/her number of visits on your web page. For example: “You have visited this site N times”. 
+ 
+
+// var visitor= "you have visisted this site 14 times"
+// alert(visitor)
+
+
+
+//  3,Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser: 
+
+// var birthyear = "my birth  year is 2000 <br>data type of my declared variable is number";
+// document.write(birthyear)
+
+
+
+// 4. A visitor visits an online clothing store 
+
+// var visitorname ="talha ordered";
+// var product= " 5tshirt"
+// var ordere = " on xyzclothing store";
+// document.write(visitorname+product+ordere);
+
+
+// chp 4
+
+// 2. Declare 5 legal & 5 illegal variable names
+
+// document.write("  RULES FOR JS VARIABLES <br>")
+// var a ="variable only contain number .$ and for example $my..1st variable<br>";
+// var b = "  Variable names can only contain with $ for example $name name or name<br> ";
+// var c="variable names are case sensitive\<br>";
+// var d = "variable names should not be js keyboards<br>";
+// document.write(a+b+c+d);
+
+// chp 5
+
+
+// 1. Write a program that take two numbers & add them in a new variable. Show the result in your browser.  
+ 
+
+// var x = 5+3;
+// document.write("sum of 3 and 5 is "+x)
+
+// Repeat task1 for subtraction, multiplication, division & modulus
+
+// var x = 5-3;
+// document.write("subtract of 3 and 5 is "+x);
+
+// var x = 5*3;
+// document.write("multiple of 3 and 5 is "+x);
+
+// var x = 5/3;
+// document.write("dividing of 3 and 5 is "+x);
+
+// var x=10;
+// var y = 20;
+
+// document.write("modulus of 10and 20 is "+x%y);
+
+// 3. Do the following using JS Mathematic Expressions 
+
+
+// var a = 5;
+// var b = ++a;
+// document.write( "value after increment"+ b)
+ 
+// var b = 7;
+// var c = --b;
+// document.write( "value after decrement is " +  c )
+
+// var a = 25;
+// var b =25;
+// var c = a+b;
+// document.write( "value after adding is " +  c )
+
+
+// var a= 25;
+// var b = 12;
+// var c = a%b;
+// document.write( "the  reminder is " +  c )
+
+// var a = 600
+// var b = a+a+a+a+a;
+// document.write ( "total cost of bying ticket  is " +  b)
+
+// 5. Write a script to display multiplication table of any 
+
+// var b = 4;
+// var c = b*1;
+// document.write ( "4*1 is " +  c +"<br>" );
+// var d = 2;
+// var x = d*b;
+// document.write ( "4*2 is " +  x +"<br>" );
+// var d = 3;
+// var f = b*d;
+// document.write ( "4*3 is " +  f +"<br>");
+ 
+// var d = 4;
+// var f = b*d;
+// document.write ( "4*4 is " +  f +"<br>");
+// var d = 5;
+// var f = b*d;
+// document.write ( "4*5 is " +  f +"<br>");
+// var d = 6;
+// var f = b*d;
+// document.write ( "4*6 is " +  f +"<br>");
+// var d = 7;
+// var f = b*d;
+// document.write ( "4*7 is " +  f +"<br>");
+// var d = 8;
+// var f = b*d;
+// document.write ( "4*8 is " +  f +"<br>");
+
+
+// var d = 9;
+// var f = b*d;
+// document.write ( "4*9 is " +  f +"<br>");
+// var d = 10;
+// var f = b*d;
+// document.write ( "4*10 is " +  f +"<br>");
+
+// 6. The Temperature Converter: 
+// var a = (-32)*5/9;
+// document.write(a);
+// var b = ("c°"* 9/5)+32;
+// document.write(b)
+
+// 7.  Write a program to implement checkout process of a 
+// var a = 650;
+// var b = 3;
+// var p2 = 100;
+// var q2 = 7;
+// var ss = 100;
+   
+// document.write("Price of item 1 is 6500 <br>")
+// document.write(" quanlityitem1 is 3 <br>")
+// document.write("Price of item 2 is 100 <br>")
+// document.write(" quanlityitem2 is 7 <br>")
+// document.write(" shiping charges 100 <br>")
+// document.write(" total cost od order is")
+// document.write(a+a+a+b+p2+q2+q2+q2+q2+q2+q2+q2+ss);
+ 
+
+// 8. Store total marks & marks obtained by a student in 2 
+
+// var t = 980;
+// var m = 804;
+// var s = 100;
+// var p = (m*s)/t
+// document.write ("total marks is 980 <br> marks obtained is 804<br> percentage is ")
+// document.write(p)
+
+// 9. Assume we have 10 US dollars & 25 Saudi Riyals. Write a 
+// var $ = 104.80;
+// var b =  28;
+// var c = $*10+b*25;
+// document.write("total currenncy in pkr .")
+// document.write(c)
+
+
+// 10. Write a program to initialize a variable with som
+// var a = 5;
+// var b = 10;
+// var c = 2;
+// var d = (5*10)/2;
+// document.write(d)
+
+// 11. The Age Calculator
+// var  a =  1992;
+// var  b = 2016;
+// var c = 24;
+// document.write("current year 2016 <br> birth year 1992 <br> your age is 24 ")
+
+// 12. The Geometrizer: Calculate properties of a circle
+
+// var a = 2;
+// var b = 3.142;
+// var c = 20;
+// var d = (a*b*c)
+// document.write("radius is 20 <br>the circumference is <br> ")
+// document.write(d);
+
+
+// 12. The Geometrizer: Calculate properties of a circle
+ 
+// var a = 3.142;
+// var b = 20;
+// var c = (a*a *b*b)
+// document.write(" <br> area is ")
+// document.write(c)
+
+
+
+// 13. The Lifetime Supply Calculator: Ever wonder how 
+
+// var a = "choclate chip ";
+// var b = 18;
+// var c = 60;
+// var d = 3;
+// document.write(" favort snaks  :" +a )
+// document.write(" <br> current age : " + b)
+// document.write(" <br>  maximum age  " + c)
+// document.write(" <br>  Amount of snacks per day " + d)
+// document.write(" <br>  You will need 150 to last you until the ripe old age of 60");
+
+
+
+
+// chp 6
+
+var a = 10;
+
+// 1. Write a program to take a number in a variable, do the 
+
+
+// document.write("the value of a is "+a);
+// var a = 10;
+// var b = ++a;
+// document.write(" <br> the value of ++a is :"+b);
+// document.write(" <br> now the value of a is :"+b);
+
+// var a= 11;
+// var b = a++;
+
+// document.write(" <br> now the value of a is :"+b);
+// document.write(" <br> now the value of a is :"+a);
+
+// var b = --a;
+
+// document.write(" <br> the value of --a is :"+b);
+// document.write(" <br> now the value of a is :"+a);
+
+
+// var b = a--;
+
+// document.write(" <br> the value of a-- is :"+b);
+// document.write(" <br> now the value of a is :"+a);
+
+
+
+
+
+// 2. What will be the output in variables a, b & result after 
+// var a = 2;
+// var b = 1;
+
+// document.write("a is "+a)
+// document.write(" <br>b is "+b)
+// document.write(" <br>result  is 3 ")
+
+
+// var a = 2;
+//  var b = 1;
+//  var c = --a - --b + ++b + b--;
+//  document.write(3)
+// document.write("<br>1")
+// document.write("<br>2")
+// document.write("<br>"+c)
+
+// 3. Write a program that takes input a name from user & greet the user 
+
+// var a = prompt("Enter your name",  "your name is");
+// alert(a);
+
+
+// 5. Write a program to take input a num
+
+// var a = +prompt("Enter your value",  "your value is");
+// var b = a*5;
+// document.write("5" +b);
+
+
+
+
+
+// var a = "english";
+// var b = "urdu";
+// var c= "math";
+// var d = 100;
+// var e = 54
+// var f = 48;
+
+// document.write("    Subject     " );
+// document.write("   total marks    ")
+// document.write("  obtained marks     ")
+// document.write( "   percentage  " )
+// document.write ("<br>" +a)
+// document.write(  ""  +      d)
+
+
+
+
+
+
+
+// chp no 11
+
+
+
+
+
+
+
+
+// 1. Write a program to take “city” name as input f
+
+// var x = prompt("City", "Enter your city name"); 
+//  if (x === "karachi")
+//  {   alert("welcome to the city of lights");  } 
+
+
+
+// 2. Write a program to take “gender” as input from user. 
+
+// var y = prompt("Gender");
+// if(y=== "male"){
+//     alert("Good morning sir")
+// }
+// if 
+// (y=== "female"){
+// alert("good morning mam")
+// }
+
+// 3. Write a program to take input color of road traffic signal 
+
+// var a = prompt("traffic signal");
+// if (a=== "red"){
+//     alert("must stop")
+// }
+// if (a=== "yellow"){
+//     alert("ready to move")
+// }
+
+// if (a==="green"){
+//     alert("move now")
+// }
+
+
+// 4. Write a program to take input remaining fuel in car 
+
+// var a = prompt("car fuel");
+// if (a>0.25 ){
+//     alert("please  refill the fuel in your car")
+// }
+
+
+
+// 5. Run this script, & check whether alert message would be 
+
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true"); 
+// }
+
+
+
+// var b = 82;
+// if(++b === 83){
+//     alert("given condition for variable a is true"); 
+// }
+
+// var c = 12; if (c++ === 13)
+// { alert("condition 1 is true"); }
+//  if (c === 13)
+//  { alert("condition 2 is true"); }
+
+
+
+//  if (++c < 14){ 
+//      alert("condition 3 is true");
+//  }
+//   if(c === 14){
+//       alert("condition 4 is true"); } 
+
+
+
+// var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost;
+//  if (totalCost === laborCost + materialCost)
+//  { alert("The cost equals"); } 
+
+// if("car" < "cat")
+// { alert("car is smaller than cat"); } 
+
+
+
+// 6. Write a program to take input the marks obtained in three 
+
+// var a = prompt("Result calculator","Enter your percentage");
+// if (a >79){
+// document.write("Grade  remarks <br> A+   Exelent")
+
+// }
+// if (a >69){
+//     document.write("Grade  remarks <br> A   Good")
+    
+//     }
+
+//     if (a >59){
+//         document.write("Grade  remarks <br> B   you need to improve")
+        
+//         }   
+
+//         if (a >49){
+//             document.write("Grade  remarks <br> F   Sorry")
+            
+//             }   
+
+// 7. Guess game: 
+
+// var x = +prompt("guess a secret number 1 to 10")
+// if(x=== 2){
+// alert("Bingo! Correct answer")
+// }
+// if (x== +1){
+//     alert("Close enough to the correct answer")
+// }
+ 
+// 8. Write a program to check whether the given number is 
+
+// if (9/3){ 
+//           alert("divsible is true");
+//       }
+       
+
+
+
+// var T = prompt("Enter temperature");
+//  if (T >40){
+//  alert("It is too hot outside");
+//  }
+ 
+
+//   else if  (T > 30){
+//     alert("The Weather today is Normal");
+//     }
+//     else if (T  > 20){
+//         alert("Today’s Weather is cool");
+//         }
+        
+        
+//        else if (T  > 10){
+//             alert("OMG! Today’s weather is so Cool.");
+//             }
+
+// 11. Write a program to c
+
+// var val1 = prompt("Enter you first value")
+// var sign = prompt("Ener your operator")
+// var val2 = prompt("Enter your last value")
+// console.log(val1+sign+val2);
+// if(sign==="+"){
+//     alert((+val1)+(+val2));
+// }
+// else if (sign==="-"){
+//     alert(val1-val2);
+// }
+// else if (sign==="*"){
+//     alert(val1*val2);
+// }
+// else if (sign=="/"){
+//     alert(val1/val2);
+// }
+// else if (sign=="%"){
+//     alert(val1%val2);
+// }
+
+// chp no 12,13
+// 1. Write a program that takes a character (number or string) 
+
+
+
+
+// var a  = prompt("Enter you number")
+// if(a<=90){
+//     alert("lower case letter")
+// }
+// if(a>=90){
+// alert("upper case letter")
+// }
+
+// 2. Write a JavaScript program that accept two integers 
+
+// var a = 5;
+// var b = 5;
+// if(a==b){
+// alert("number is equal");
+// }
+// else if (a > b){
+//     alert("Number 5 is larger")
+// }
+            
+
+// 3. Write a  positive, negative or zero. 
+ 
+
+// var a  = +prompt("Enter you number")
+// if(a===-5){
+//     alert("NUmber is negative")
+// }
+// if(a===5){
+// alert("Number is positive")
+// }
+
+
+// 4. Write a program that takes a ch
+
+// var a = prompt("Enter Vowel ")
+// if(a==="aeiou"){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+
+// 5. Write a program that a. Store correct password in a JS variable
+
+// var a = prompt("Enter your password")
+// if(a==="human123"){
+//     alert("correct password")
+
+// }
+
+// else if  (a=="human1234"){
+//     alert("correct password")
+// }
+// else{
+//     alert("incorrect password")
+// }
+
+
+
+// 6. This if/else statement does not work
+
+// var hour = 13;
+//  if (hour < 18) {  alert("Good morning ")}
+//   else if(hour>18) {
+//     alert("good Evening")
+// }
+ 
+ 
+
+// chp no  13 15
+
+// 1. Declare an empty array using JS literal 
+
+// var studientsname = ["", "", "", ""];
+
+
+
+// 3. Declare and initialize a strings array. 
+
+
+// var arr = ["this", "is", "my", "cat"];
+
+
+// Declare and initialize a numbers array.
+
+// var a = ["1", "2", "3", "4"];
+
+// 7. Declare and Initialize an array and store available 
+
+// var a = ["Qualification", "<br>","ssc","<br>","Hcs","<br>","Bcs","<br>","Bs","<br>","Bcom","<br>","MS","<br>","Mphil","<br>","phd"]
+// document.write(a);
+
+
+
+// 8. Write a program to store 3 student names in an array.Take 
+
+// var st =[" ali ", " usman"," Talha"]
+// var n = [ 320, 230, 480];
+// var p = [" percentage 64%"," Percentage 96%"," Percentage 46%"];
+// document.write(st[0] +n[0] +p[0]+"<br>"+st[1]+n[1] +p[2]+"<br>"+st[2] +n[2] +p[1]+"<br>")
+
+
+// 9. Initialize an array with color names.
+
+var colour = [""]
+
+colour[0]= ["white"]
+colour[1]=["red"]
+colour[2]=["blue"]
+console.log(colour)
+
+// a. Ask the user what color he/she wants to add to the beginning 
+
+
+
+colour.unshift("green")
+console.log(colour)
+
+// b. Ask the user what color he/she wants to add to the end 
+
+
+colour.push("purple");
+console.log(colour)
+
+
+// c. Add two more color to the beginning of the array. 
+
+
+colour.unshift("wheat","light blue");
+console.log(colour)
+
+
+// d. Delete the first color in the array. 
+colour.shift();
+console.log(colour);
+
+
+
+// e. Delete the last color in the array
+
+
+colour.pop();
+console.log(colour);
+
+
+
+// f. Ask the user at which index he/she wants to add a color 
+
+
+colour.splice(2,0,"red")
+console.log(colour);
